b2562b480d03ffd1deb862b69f3ce06b7e32c48d
Developer Siddarth <142804820+Siddarth-abcs@users.noreply.github.com>
2024-03-07 15:51:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import { useState } from 'react'
-// import { Blogpreviewcardmain } from './blog-preview-card-main/Blogpreviewcardmain'
-import { Recipepagemain } from './recipe-page-main/Recipepagemain'
+import { Blogpreviewcardmain } from './blog-preview-card-main/Blogpreviewcardmain'
+//import { Recipepagemain } from './recipe-page-main/Recipepagemain'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    {/* <Blogpreviewcardmain/> */}
-    <Recipepagemain/>
+    <Blogpreviewcardmain/>
+    
     </>
   )
 }
